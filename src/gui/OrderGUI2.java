0484db238d2f637dd13929f1b27b1191613163b5
JowitaKK <jowita.knap@wp.pl>
2019-02-10 15:15:11 +0000

    dramat gui

--- src/gui/OrderGUI2.java
+++ src/gui/OrderGUI2.java
@@ -5,11 +5,16 @@ import domain.OrderList;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.Font;
 
 
 public class OrderGUI2 extends JFrame implements ActionListener{
     private MenuList items;
     private OrderList orders;
+
+    private Font labelFont = new Font("Serif", Font.BOLD, 30);
+    private Border border = BorderFactory.createLineBorder(Color.BLACK);
 
     public OrderGUI2(MenuList items, OrderList orders) {
         this.items = items;
@@ -22,7 +27,6 @@ public class OrderGUI2 extends JFrame implements ActionListener{
      * Initialize GUI
      */
     public void initGUI() {
-        setTitle("Coffee Menu");
 
         setupNorthPanel();
         setupCenterPanel();
@@ -35,16 +39,24 @@ public class OrderGUI2 extends JFrame implements ActionListener{
     }
 
     private void setupNorthPanel() {
-        JPanel northPanel = new JPanel(new BorderLayout());
-        JLabel title = new JLabel("Bar");
-        northPanel.add(title, BorderLayout.WEST);
+        JPanel northPanel = new JPanel();
+
+        northPanel.setLayout(new BorderLayout());
+        northPanel.setPreferredSize(new Dimension(1024, 120));
+        northPanel.setBorder(border);
+        northPanel.setBackground(Color.WHITE);
+
+        JLabel label = new JLabel("Coffee Bar Menu");
+        label.setFont(labelFont);
+        northPanel.add(label, BorderLayout.WEST);
+
         this.add(northPanel, BorderLayout.NORTH);
     }
 
     private JPanel createMenuPanel(){
         JPanel menuPanel = new JPanel();
         menuPanel.setLayout(new GridBagLayout());
-        menuPanel.setBackground(Color.RED);
+        menuPanel.setBackground(Color.WHITE);
 
         JButton showHotDrinks = new JButton("Hot Drinks");
         showHotDrinks.addActionListener(this);
@@ -75,11 +87,11 @@ public class OrderGUI2 extends JFrame implements ActionListener{
         GridBagConstraints c3 = new GridBagConstraints();
         c3.fill = GridBagConstraints.BOTH;
         c3.gridwidth = 1;
-        c3.weightx = 1;
+        c3.weightx = 0.5;
         c3.weighty = 1;
         c3.gridx = 0;
         c3.gridy = 1;
-        c3.insets = new Insets(30,30,30,30);
+        c3.insets = new Insets(70,70,70,70);
         menuPanel.add(showMeal, c3);
 
         JButton showDessert = new JButton("Dessert");
@@ -87,53 +99,70 @@ public class OrderGUI2 extends JFrame implements ActionListener{
         GridBagConstraints c4 = new GridBagConstraints();
         c4.fill = GridBagConstraints.BOTH;
         c4.gridwidth = 1;
-        c4.weightx = 1;
+        c4.weightx = 0.5;
         c4.weighty = 1;
         c4.gridx = 1;
         c4.gridy = 1;
-        c4.insets = new Insets(30,30,30,30);
+        c4.insets = new Insets(70,70,70,70);
         menuPanel.add(showDessert, c4);
+
+        JPanel offerPanel = new JPanel();
+        offerPanel.setLayout(new GridBagLayout());
+        showDessert.addActionListener(this);
+        GridBagConstraints o0 = new GridBagConstraints();
+        o0.fill = GridBagConstraints.BOTH;
+        o0.gridwidth = 2;
+        o0.weightx = 0.5;
+        o0.weighty = 1;
+        o0.gridy = 2;
 
         JButton showOffer1 = new JButton("Offer 1");
         showOffer1.addActionListener(this);
-        GridBagConstraints c1 = new GridBagConstraints();
-        c1.fill = GridBagConstraints.BOTH;
-        c1.gridwidth = 1;
-        c1.weightx = 1;
-        c1.weighty = 2;
-        c1.gridx = 0;
-        c1.gridy = 0;
-        menuPanel.add(showOffer1, c1);
+        GridBagConstraints o1 = new GridBagConstraints();
+        o1.fill = GridBagConstraints.BOTH;
+        o1.gridwidth = 1;
+        o1.weightx =  0.25;
+        o1.weighty = 1;
+        o1.gridx = 0;
+        o1.gridy = 0;
+        offerPanel.add(showOffer1, o1);
 
         JButton showOffer2 = new JButton("Offer 2");
         showOffer2.addActionListener(this);
-        GridBagConstraints c2 = new GridBagConstraints();
-        c2.fill = GridBagConstraints.BOTH;
-        c2.gridwidth = 1;
-        c2.weightx = 1;
-        c2.weighty = 1;
-        c2.gridx = 1;
-        c2.gridy = 0;
-//        southPanel.add(showOffer2, c2);
+        GridBagConstraints o2 = new GridBagConstraints();
+        o2.fill = GridBagConstraints.BOTH;
+        o2.gridwidth = 1;
+        o2.weightx =  0.25;
+        o2.weighty = 1;
+        o2.gridx = 1;
+        o2.gridy = 0;
+        offerPanel.add(showOffer2, o2);
 
-        JButton showOffer3= new JButton("Offer 3");
+        JButton showOffer3 = new JButton("Offer 3");
         showOffer3.addActionListener(this);
-        GridBagConstraints c3 = new GridBagConstraints();
-        c3.fill = GridBagConstraints.BOTH;
-        c3.gridwidth = 1;
-        c3.weightx = 1;
-        c3.weighty = 1;
-        c3.gridx = 2;
-        c3.gridy = 0;
-//        southPanel.add(showOffer3, c3);
+        GridBagConstraints o3 = new GridBagConstraints();
+        o3.fill = GridBagConstraints.BOTH;
+        o3.gridwidth = 1;
+        o3.weightx =  0.25;
+        o3.weighty = 1;
+        o3.gridx = 2;
+        o3.gridy = 0;
+        offerPanel.add(showOffer3, o3);
+
+        menuPanel.add(offerPanel, o0);
 
         return menuPanel;
     }
 
     private JPanel createBasketPanel(){
         JPanel basketPanel = new JPanel();
-        basketPanel.setLayout(new GridBagLayout());
+        basketPanel.setLayout(new BorderLayout());
         basketPanel.setBackground(Color.WHITE);
+        basketPanel.setBorder(border);
+
+        JLabel label = new JLabel("Basket");
+        label.setFont(labelFont);
+        basketPanel.add(label, BorderLayout.NORTH);
 
         return basketPanel;
     }
@@ -157,11 +186,13 @@ public class OrderGUI2 extends JFrame implements ActionListener{
         GridBagConstraints c2 = new GridBagConstraints();
         c2.fill = GridBagConstraints.BOTH;
         c2.gridwidth = 1;
+        c2.gridheight = 1;
         c2.weightx = 6;
         c2.weighty = 1;
         c2.gridx = 1;
         c2.gridy = 0;
         centerPanel.add(basketPanel, c2);
+
 
         this.add(centerPanel, BorderLayout.CENTER);
     }
